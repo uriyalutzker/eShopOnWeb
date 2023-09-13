809545b39fea65af34a24624e6333624f1c6e2db
uriyalutzker <uriya@bigfish.co.il>
2023-09-13 11:48:28 +0300

    added comments

--- src/Web/Constants.cs
+++ src/Web/Constants.cs
@@ -4,6 +4,7 @@ public static class Constants
 {
     public const string BASKET_COOKIENAME = "eShop";
     public const int ITEMS_PER_PAGE = 10;
-    public const string DEFAULT_USERNAME = "Guest";
+    public const string DEFAULT_USERNAME = "User";
     public const string BASKET_ID = "BasketId";
 }
+// third change
